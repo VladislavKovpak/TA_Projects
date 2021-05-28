763f36a4a136c73bdc675fcb7422c7445222c4a9
HP-Notebook <HP-Notebook@DESKTOP-1HTOPND>
2021-05-28 08:51:35 +0300

    Lab8

--- src/test/java/knu/fit/ist/ta/Lab8Tests.java
+++ src/test/java/knu/fit/ist/ta/Lab8Tests.java
@@ -4,10 +4,15 @@
  * and open the template in the editor.
  */
 package knu.fit.ist.ta;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import knu.fit.ist.ta.lab5.LinearSearch;
 import knu.fit.ist.ta.lab8.AlgorithmDetails;
+import knu.fit.ist.ta.lab8.Task;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  *
@@ -15,6 +20,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 @SpringBootTest
 public class Lab8Tests {
-  
-        
+   @Test
+     void test1(){
+            List<Integer> list = new ArrayList<>();
+            list.addAll(Arrays.asList(-2,-1,0,1,2,3,4,5,6,7,8,9,10));
+            
+            assertEquals(-1,LinearSearch.linearSearch(32,list));
+            assertEquals(-1,LinearSearch.linearSearch(-8,list));
+        }   
+   @Test
+     void test2(){
+            List<Integer> list = new ArrayList<>();
+            list.addAll(Arrays.asList(-3,-5,0,3,3,3,8,5,6,7,8,9,10));
+            
+            assertEquals(-1,LinearSearch.linearSearch(32,list));
+            assertEquals(-1,LinearSearch.linearSearch(-8,list));
+        }   
 }
--- src/test/java/knu/fit/ist/ta/TaApplicationTests.java
+++ src/test/java/knu/fit/ist/ta/TaApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TaApplicationTests {
 
-     @Autowired
+    @Autowired
     EquationSample equationSample;
 
     @Test
